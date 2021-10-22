a17fca6f7b57144ee1461396242a31027b96c1bf
Lourdes Benites <lourdesben95@gmail.com>
2021-10-22 16:47:08 -0600

    Vehicle edit

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var vehicle = {
+make: "Honda",
+model: "Accord",
+year: 1995,
+miles: 150,000,
+twoDoor: true,
+recentOilChange: false,
+uniqueFeatures: ["heated seats", "stick shift"],
+colorOptions: ["white", "gold", "blue"]
+}
