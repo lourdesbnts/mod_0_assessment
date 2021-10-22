0cc38ff6fff876972ce665476c0a57d2fce3f85d
Lourdes Benites <lourdesben95@gmail.com>
2021-10-22 16:22:19 -0600

    Complete program specifoc challenge

---
+++ variable.js
@@ -0,0 +1,11 @@
+var famousPerson = 'Kanye West';
+var isActive = true;
+famousPerson = 'Betty White';
+var num3 = 4;
+var num9 = 4000;
+console.log(num3 + num9);
+var sum = 4004;
+var animals = ['owl', 'elephant', 'whale'];
+animals.splice('owl');
+var count = 0;
+console.log(famousPerson + count)
