98ddda438f8f49e8f90b5fa2297a92f441de151c
Lourdes Benites <lourdesben95@gmail.com>
2021-10-22 16:36:09 -0600

    Airport edit

---
+++ models/modelA.js
@@ -0,0 +1,11 @@
+var airport = {
+name: "John Wayne";
+city: "Santa Ana";
+airportCapacity: 3000;
+numberOfFlights: 200;
+internationalFlights: false;
+servesAlcohol: true;
+popularDestinations: ["New York", "Florida", "California"]
+foodOptions: ["Burger King", "Wendy's", "Starbucks"]
+
+}
